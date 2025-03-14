96d54881d6e682f81a8a3fd048ff24c916ecc5d2
“leejiyun1” <poi20701556@gmail.com>
2025-03-14 21:17:53 +0900

    🛠 Git 파일 오류 수정 및 정리

---
+++ 01-Python-Basics/assignment/7day/bomul.py
@@ -0,0 +1,18 @@
+import random
+
+# 게임 초기화
+def initialize_game(n):
+
+# 거리 계산
+def calculate_distance(treasure_position, player_position):
+
+# 플레이어 이동
+def move_player(board_size, player_position, direction):
+
+# 게임 실행
+def play_game(board_size):
+
+# 게임 보드 크기 설정 및 게임 시작
+if __name__ == "__main__":
+    board_size = 5  # 보드 크기를 5x5로 설정
+    play_game(board_size)

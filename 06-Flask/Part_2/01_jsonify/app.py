6d79b45d8be27a6f90894f82608aae0fb571dca8
“leejiyun1” <poi20701556@gmail.com>
2025-04-18 18:10:52 +0900

    ✅ 2_day_study
    2일차 공부 완료

---
+++ 06-Flask/Part_2/01_jsonify/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/v1/feeds', methods=['GET'])
+def show_all_feeds():
+    # return jsonify({'result':'success', 'data': {"feed1":"data", "feed2":"data2"}})
+	return {'result':'success', 'data': {"feed1":"data", "feed2":"data2"}}
+
+@app.route('/api/v1/feeds/<int:feed_id>', methods=['GET'])
+def show_one_feed(feed_id):
+    print(feed_id)
+    return jsonify({'result':'success', 'data': {"feed1":"data"}})
+
+@app.route('/api/v1/feeds', methods=['POST'])
+def create_one_feed():
+    name = request.form['name']
+    age = request.form['age']
+    print(name, age)
+    return jsonify({'result':'success'})
+
+datas = [{"name": "item1", "price": 10}]
+
+@app.get("/datas")
+def get_datas():
+    return {'datas':datas}
+
+@app.post("/datas")
+def create_data():
+    request_data = request.get_json()
+
+    new_data = {'items': request_data.get("items", [])}
+    datas.append(new_data)
+    return new_data, 201
